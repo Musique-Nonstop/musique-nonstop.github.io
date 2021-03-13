eeb627abc7e1cea990ada91858695ec00e0f7a23
sambodjango <gringolalia@gmail.com>
2021-03-13 16:24:09 -0300

    deploy

---
+++ precache-manifest.323541c69daabc5333736c40eaecdb8a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5fa1cc7d3f78615c355",
+    "url": "/css/about.885c8abf.css"
+  },
+  {
+    "revision": "63f6c96cddb7897154ea",
+    "url": "/css/app.5169ce6c.css"
+  },
+  {
+    "revision": "61e18425bceb7c30850b",
+    "url": "/css/chunk-vendors.3c76955e.css"
+  },
+  {
+    "revision": "46f7ca377d6681b4917a2bf66a47a5e8",
+    "url": "/img/cathar-crucigrama.46f7ca37.svg"
+  },
+  {
+    "revision": "7226a46e7f28a60e289da005060e2db6",
+    "url": "/img/enigma.7226a46e.jpg"
+  },
+  {
+    "revision": "ec885583295e5abd38e107ea9ac4eb4e",
+    "url": "/img/nyc-city-hall-station.ec885583.jpg"
+  },
+  {
+    "revision": "d221a8beea4038e2cfe64c061ed7a630",
+    "url": "/img/nyc-el.d221a8be.jpg"
+  },
+  {
+    "revision": "e123b07e2a7a92c86ac3f9d1d427f5b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5fa1cc7d3f78615c355",
+    "url": "/js/about.f65b571a.js"
+  },
+  {
+    "revision": "63f6c96cddb7897154ea",
+    "url": "/js/app.3a0f0f19.js"
+  },
+  {
+    "revision": "61e18425bceb7c30850b",
+    "url": "/js/chunk-vendors.190952d3.js"
+  },
+  {
+    "revision": "0616d7a961e326bb5f6df1385889d3a7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
